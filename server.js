50343537c44331b618fc3c3083785d328e84e666
Craig Barkley <craig@iowastartupaccelerator.com>
2020-08-04 10:28:33 -0500

    add env and port is listening

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
 'use strict';
+
+//Set up Express Server
+const express = require('express');
+const app = express();
+
+
+//Environmental Variables
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+app.get( '*', (request, response) => response.status(404).send('This request route does not exsist. Bye for now.'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server.js is Listening on PORT ${PORT}`));
