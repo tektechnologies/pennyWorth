98b58255a5647e22d6673af31b64081997d42de4
Craig Barkley <craig@iowastartupaccelerator.com>
2020-08-04 13:54:24 -0500

    add server rendering list and index routes

--- server.js
+++ server.js
@@ -3,10 +3,39 @@
 //Set up Express Server
 const express = require('express');
 const app = express();
-
+const user = {username: 'My Lord'};
 
 //Environmental Variables
 const PORT = process.env.PORT || 3000;
+//Middle Wear
+//Wire up Static files from the public folder
+app.use(express.static('./public'));
+
+
+
+
+//Page Routes
+app.set('view engine', 'ejs');
+
+app.get('/index', (request, response) => {
+  let viewModel = {
+    user,
+  }
+  response.render('index', viewModel);
+});
+
+
+//Add route for Task List Page
+app.get('/taskList', (request, response) => {
+  let viewModel = {
+    user,
+  }
+  response.render('taskList', viewModel);
+});
+
+
+
+
 
 
 
